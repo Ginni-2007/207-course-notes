9f5b1dd17828268d8103ba064d27aab902a7a5f8
ak_ginni <ak.grishma@gmail.com>
2026-09-20 19:55:23 -0400

    reformatted ex. 5

--- exercises/ex05-cleanup/src/main/java/Rectangle.java
+++ exercises/ex05-cleanup/src/main/java/Rectangle.java
@@ -2,28 +2,26 @@ public class Rectangle {
     private double width;
     private double height;
 
-    public Rectangle(double w,double h){
-        this.width=w;
-        this.height=h;
+    public Rectangle(double w, double h) {
+        this.width = w;
+        this.height = h;
     }
 
-    public double area(){
-        return width*height;
+    public double area() {
+        return width * height;
     }
 
     /**
      * scales the rectangle
+     *
      * @param factor
      */
     public void scale(double factor) {
-      width = width * factor;
-      height = height * factor;
+        width = width * factor;
+        height = height * factor;
     }
 
-    public boolean isLargerThan(Rectangle other){
-        if(area() > other.area())
-            return true;
-        else
-            return false;
+    public boolean isLargerThan(Rectangle other) {
+        return area() > other.area();
     }
 }
